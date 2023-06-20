293649eb0dc2e1f5db5487d27fa0b56236a42afd
KatiaSonally <katiasonally@yahoo.com.br>
2023-06-20 11:30:45 -0300

    feat: list all students enrollments

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const PessoaController = require('../controllers/PessoaController')
+
+const router = Router()
+
+router
+  .get('/pessoas', PessoaController.pegaPessoasAtivas)
+  .get('/pessoas/todos', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+  .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+  .get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+  .post('/pessoas', PessoaController.criaPessoa)
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+  .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+  .put('/pessoas/:id', PessoaController.atualizaPessoa)
+  .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
+  .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
+  .delete('/pessoas/:id', PessoaController.apagaPessoa)
+  
+module.exports = router
---
+++ readme.md
@@ -0,0 +1,17 @@
+Projeto desenvolvido no curso de ORM com NodeJS da Alura para a trilha Full Stack do Programa Desenvolve do O Boticário.
+
+O projeto: 
+
+Construir uma API para um sistema de controle de alunos e turmas de uma escola de inglês a partir de um diagrama de banco, onde as tabelas já estão dadas.
+
+Os recursos: 
+
+ORM Sequelize com o banco MySQL e testes no Postman.
+
+O objetivo: 
+
+Utilizar JavaScript para conectar e passar instruções para o banco.
+
+Utilizar as ferramentas do Sequelize e para fazer o CRUD junto ao banco e devolver dados no formato JSON. O Sequelize torna a aplicação mais legível, padronizada e de mais fácil manutenção. 
+
+Organizar a aplicação no padrão MVC.
