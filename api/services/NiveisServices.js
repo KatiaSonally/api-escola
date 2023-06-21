cbca8c98ef8a5429c933e03e08b838ec7551e5a6
KatiaSonally <katiasonally@yahoo.com.br>
2023-06-21 15:38:36 -0300

    refactor: creating services for people, classes and levels

---
+++ api/services/NiveisServices.js
@@ -0,0 +1,10 @@
+const Services = require('./Services')
+
+class NiveisServices extends Services {
+    constructor() {
+        super ('Niveis')
+    }
+    //métodos específicos do controlador de niveis
+}
+
+module.exports = NiveisServices
