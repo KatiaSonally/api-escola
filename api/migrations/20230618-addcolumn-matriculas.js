3aac56c3bdf6622c3c3a6b2b76f6a235d71eb408
KatiaSonally <katiasonally@yahoo.com.br>
2023-06-21 08:58:23 -0300

    feat: search open classes by date range

--- api/migrations/20230618-addcolumn-matriculas.js
+++ api/migrations/20230618-addcolumn-matriculas.js
@@ -2,12 +2,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn('Matriculas', 'deletedAt', {
+        await queryInterface.addColumn('Pessoas', 'deletedAt', {
             allowNull: true,
             type: Sequelize.DATE
             });
     },
   async down(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('Matriculas', 'deletedAt');
+        await queryInterface.removeColumn('Pessoas', 'deletedAt');
     }
 };
